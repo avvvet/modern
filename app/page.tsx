f1898fbdd3cf85608c96618305cadb899711b4d0
Awet Tsegazeab <avvvet@gmail.com>
2024-02-11 22:45:04 -0500

    search filter layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-import { Hero } from "@/components"
+import { CustomFilter, Hero, SearchBar } from "@/components"
 
 export default function Home() {
   return(
     <main className="overflow-hidden">
-      <Hero></Hero>
+      <Hero/>
+
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4x1 font-extrabold">Gemstone category</h1>
+          <p>Explore the gemstone you might like</p>
+        </div>
+
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
---
+++ components/SearchBar.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+const SearchBar = () => {
+  const handleSearch = () => {}
+
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+        <div className="searchbar__item">
+            
+        </div>
+    </form>
+  )
+}
+
+export default SearchBar
