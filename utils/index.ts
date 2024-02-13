74a104706f4fa6f195fd6c2a0b37eb6a599a2c18
Awet Tsegazeab <avvvet@gmail.com>
2024-02-13 12:32:53 -0500

    remove key

--- utils/index.ts
+++ utils/index.ts
@@ -6,7 +6,7 @@ export async function fetchCars() {
   const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': '46b5d3255cmsh76ab153b7f735d0p1bbd6ejsnd8d71d0c1239',
+      'X-RapidAPI-Key': '',
       'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
   };
